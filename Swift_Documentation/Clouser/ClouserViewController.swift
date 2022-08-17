018e3745f974291e084aaaf89a16712bf7b7b028
Zhora212001 <zhora.babakhanyan@ifixgroup.com>
2022-08-17 17:43:25 +0400

    Initial Commit

---
+++ Swift_Documentation/Clouser/ClouserViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ClouserViewController.swift
+//  Swift_Documentation
+//
+//  Created by Zhora Babakhanyan on 8/17/22.
+//
+
+import UIKit
+
+class ClouserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ Swift_Documentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift_Documentation
+//
+//  Created by Zhora Babakhanyan on 8/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
